d18a1d86b29bd3e492e0611c16c8bfbc5744244a
Kunduz <talaybekovna.kunduz@gmail.com>
2021-03-30 18:04:26 +0600

    added homework21

---
+++ app/src/main/java/kg/tutorialapp/homework/hw21.kt
@@ -0,0 +1,11 @@
+package kg.tutorialapp.homework
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class hw21 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hw21)
+    }
+}
